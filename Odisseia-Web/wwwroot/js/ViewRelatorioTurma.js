04ceb4fbd637f57123d4a5da2eb36c1d2e577c07
ArthurpimentaBraga <pimentabraga2@gmail.com>
2019-10-20 13:02:27 -0300

    Dropdown Missao sidebar // Modificação pagina _View_Relatorio_Turma

---
+++ Odisseia-Web/wwwroot/js/ViewRelatorioTurma.js
@@ -0,0 +1,8 @@
+﻿function ShowResultStudent() {
+    var x = document.getElementById("ResultStudent");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
