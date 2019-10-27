977e8608e21e01e7b40d7d3ceea0f2322ca7ba71
Rafael Marotta <rafaelmarottag@gmail.com>
2019-10-27 16:15:14 -0300

    Adicionando titulo ao relatório de missão por turma

--- Odisseia-Web/Models/Relatorio/BasicReportClassDTO.cs
+++ Odisseia-Web/Models/Relatorio/BasicReportClassDTO.cs
@@ -5,6 +5,7 @@ namespace Models.Relatorio
     public class BasicReportClassDTO
     {
         public int Id { get; set; }
+        public string Title { get; set; }
         public double RightAverage { get; set; }
         public double WrongAverage { get; set; }
         public TimeSpan TimeAverage { get; set; }
